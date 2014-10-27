1e80d043f008ddd6176767f131e9aa6c7485c491
Anush Krishnan <k.anush@gmail.com>
2014-10-27 13:46:17 -0400

    Clean up code and add more features.
    * Bug fix: Do not count lines where people are removed or added.
    * Accept the Whatsapp history file as a command line input.
    * Output the day when maximum messages were sent.
    * Plot number of messages sent each day.
    * Make graphs look nicer.
    * Use raw strings for regexes.

--- wru.py
+++ wru.py
@@ -1,26 +1,37 @@
 #!/usr/bin/python
 import re
-from datetime import datetime
+import datetime as dt
+import matplotlib
+matplotlib.use('Agg')
 import matplotlib.pyplot as plt
 import numpy as np
+import argparse
+
+# command line arguments
+parser = argparse.ArgumentParser(description='Analyse Whatsapp conversations', formatter_class= argparse.ArgumentDefaultsHelpFormatter)
+parser.add_argument('-file', dest='inputFile', default='wru.txt', help='File containing Whatsapp history')
+args = parser.parse_args()
 
 totalMessagesEachDay = []
 dailyTotalMessages = np.zeros(7)
 dayFrequency = np.zeros(7, dtype=int)
 firstDate = None
-for f in open("wru_test.txt"):
-	a = re.split('[ ,]', f)
+for f in open(args.inputFile):
+	a = re.split(r'[ ,]', f)
 	dateString=''
+	# get the date in a format that python can understand
 	try:
-		if re.match('[A-Z][a-z][a-z]', a[0]) and re.match('[0-9]|[0-9][0-9]', a[1]):
-			if re.match('[0-9][0-9][0-9][0-9]', a[3]):
-				dateString = a[0] + ' ' + a[1] + ' ' + a[3]
+		lastTwoWords = "{} {}".format(a[-2], a[-1])
+		if re.match(r'[A-Z][a-z][a-z]', a[0]) and re.match(r'[0-9]|[0-9][0-9]', a[1]) and a[-1]!='joined\n' and lastTwoWords!='was removed\n':
+			if re.match(r'[0-9][0-9][0-9][0-9]', a[3]):
+				dateString = "{} {} {}".format(a[0], a[1], a[3])
 			else:
-				dateString = a[0] + ' ' + a[1] + ' ' + datetime.now().year
+				dateString = "{} {} {}".format(a[0], a[1], dt.datetime.now().year)
 	except:
 		pass
+
 	if dateString:
-		dateObject = datetime.strptime(dateString, '%b %d %Y')
+		dateObject = dt.datetime.strptime(dateString, '%b %d %Y')
 		if not firstDate:
 			firstDate = dateObject
 			currentDate = dateObject
@@ -43,17 +54,31 @@ dayFrequency[dayOfWeek]+=1
 totalMessagesEachDay.append(curNumber)
 
 dailyAvgMessages = dailyTotalMessages/dayFrequency
-print "Total days: {}".format(len(totalMessagesEachDay))
-print "Days on which at least one message was sent: {}".format(dayFrequency.sum())
-np.set_printoptions(2)
-print "Average number of messages during each day of the week:\n{}".format(dailyAvgMessages)
+totalDays = len(totalMessagesEachDay)
+dates = [firstDate+dt.timedelta(days=x) for x in range(totalDays)]
+labels = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"]
 
+# command line output
+print "Total days: {}".format(totalDays)
+print "Number of days when at least one message was sent: {}".format(dayFrequency.sum())
+dayOfMaxMessages = np.argmax(totalMessagesEachDay)
+print "Maximum messages: {} on {} ({})".format(totalMessagesEachDay[dayOfMaxMessages], dt.datetime.strftime(dates[dayOfMaxMessages], "%b %d, %Y"), labels[dates[dayOfMaxMessages].weekday()])
+
+# plot average daily messages
 days = np.arange(7, dtype=int)
-labels = ["Mon", "Tue","Wed","Thur","Fri","Sat","Sun",]
-plt.bar(days, dailyAvgMessages)
-plt.title('Average number of messages per day on Wru since {}'.format(firstDate))
+plt.clf()
+plt.bar(days, dailyAvgMessages, align='center')
+plt.title('For the period from {} to {}\n(counting only days when messages were sent)'.format(dt.datetime.strftime(firstDate, "%b %d, %Y"), dt.datetime.strftime(currentDate, "%b %d, %Y")))
 plt.xlabel('Day of week')
 plt.ylabel('Average number of messages')
 plt.xticks(days, labels)
-#plt.show()
-plt.savefig("output.png")
+plt.tight_layout()
+plt.savefig("dailyAvgMessages.png")
+
+# plot total messages each day
+plt.clf()
+plt.bar(dates, totalMessagesEachDay, align='center')
+plt.ylabel('Number of messages')
+plt.xticks(rotation=70)
+plt.tight_layout()
+plt.savefig("totalMessagesEachDay.png")
